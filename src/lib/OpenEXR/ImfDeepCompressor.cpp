321e6cc71c5a10c981f715a2d48f5984c8aceb6c
Vlad-Andrei Lazar <vladal@unity3d.com>
2023-11-06 08:55:34 -0500

    Blosc managed init/delete

--- src/lib/OpenEXR/ImfDeepCompressor.cpp
+++ src/lib/OpenEXR/ImfDeepCompressor.cpp
@@ -29,11 +29,26 @@ ZSTD_uncompress_impl (const char* inPtr, int inSize, Imf::DeepCompressor::raw_pt
     return dSize;
 }
 
+class BloscInit
+{
+public:
+    static void Init () { getInstance (); }
+    BloscInit (const BloscInit&) = delete;
+    BloscInit& operator= (const BloscInit&) = delete;
+private:
+    BloscInit () { blosc2_init (); }
+    ~BloscInit () { blosc2_destroy (); }
+    static BloscInit& getInstance ()
+    {
+        static BloscInit instance;
+        return instance;
+    }
+};
 
 int
 BLOSC_compress_impl (const char* inPtr, int inSize, Imf::DeepCompressor::raw_ptr& outPtr)
 {
-    blosc2_init();
+    BloscInit::Init();
     blosc2_cparams cparams = BLOSC2_CPARAMS_DEFAULTS;
 
     cparams.typesize = sizeof(int32_t);
@@ -53,7 +68,6 @@ BLOSC_compress_impl (const char* inPtr, int inSize, Imf::DeepCompressor::raw_ptr
     outPtr = Imf::DeepCompressor::raw_ptr((char*)buffer, &free);
 
     blosc2_schunk_free(schunk);
-    blosc2_destroy();
 
     return size;
 }
@@ -61,21 +75,17 @@ BLOSC_compress_impl (const char* inPtr, int inSize, Imf::DeepCompressor::raw_ptr
 int
 BLOSC_uncompress_impl (const char* inPtr, int inSize, Imf::DeepCompressor::raw_ptr& outPtr, int maxScanLineSize)
 {
-    blosc2_init();
-
     blosc2_dparams dparams = BLOSC2_DPARAMS_DEFAULTS;
     dparams.nthreads = ILMTHREAD_NAMESPACE::ThreadPool::globalThreadPool ().numThreads();;
 
     auto in = const_cast<char*>(inPtr);
-    blosc2_schunk* schunk = blosc2_schunk_from_buffer(reinterpret_cast<uint8_t*>(in), inSize, false);//(schunk, in, inSize);
+    blosc2_schunk* schunk = blosc2_schunk_from_buffer(reinterpret_cast<uint8_t*>(in), inSize, false);
 
     auto buffSize = maxScanLineSize * 256;
     outPtr = Imf::DeepCompressor::raw_ptr((char*)malloc (buffSize), &free);
     auto size = blosc2_schunk_decompress_chunk(schunk, 0, outPtr.get(), buffSize);
 
     blosc2_schunk_free(schunk);
-    blosc2_destroy();
-
     return size;
 }
 

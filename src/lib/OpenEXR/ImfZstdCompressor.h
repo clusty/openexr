5cbd94127e99f2847e6aa88569e425c2bb1b87a4
Vlad-Andrei Lazar <vladal@unity3d.com>
2024-01-15 10:17:56 -0500

    Brand new spanking blosc build.

--- src/lib/OpenEXR/ImfZstdCompressor.h
+++ src/lib/OpenEXR/ImfZstdCompressor.h
@@ -5,11 +5,6 @@
 
 #pragma once
 
-//
-// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) Contributors to the OpenEXR Project.
-//
-
 #include <memory>
 #include "ImfNamespace.h"
 #include "ImfCompressor.h"
